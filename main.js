52fb2f382e176c724d8d624259f7bff746f89e21
yakovina <sasha.yakovina@gmail.com>
2018-06-13 12:55:35 +0300

    added different cats

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var cat_temp= document.getElementById('tempcat');
 var cat = cat_temp.content.cloneNode(true);
-console.log(cat);
-
 var grid= document.querySelector('.catGrid');
+
+
+var numberOfCats = prompt('Введите количество котов', 10);
+
+if (isNaN(numberOfCats)) {
+  alert( "Вы ввели не число. Выведем 10 котов" );
+  numberOfCats=10;
+} else {
+  numberOfCats = +numberOfCats;
+}
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://ma-cats-api.herokuapp.com/api/cats', true);
@@ -11,28 +19,32 @@ request.send();
 
 
 
-
-
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
-    
+    var myCats= [];
     var data = JSON.parse(request.responseText);
-    console.log(data[0]);
-    for (var i=0; i<20; i++){
-        var cat = cat_temp.content.cloneNode(true);
-        grid.appendChild(cat);
-    }
-    
+    for (var i=0; i<numberOfCats; i++){
+        myCats[i] = cat_temp.content.cloneNode(true);
+        myCats[i].querySelector('.cat__image').src = data.cats[i].img_url;
+        grid.appendChild(myCats[i]);
 
+    }
 
   } else {
     // We reached our target server, but it returned an error
-
   }
 };
 
-request.onerror = function() {
-  // There was a connection error of some sort
+request.onerror = function() {// There was a connection error of some sort
 };
 
 
+function makeCounter() {
+  var currentCount = 1;
+
+  return function() {
+    return currentCount++;
+  };
+}
+
+var counter = makeCounter();
